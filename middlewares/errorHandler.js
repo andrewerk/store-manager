61c1459bc37c9bd800c95bc3624b3c2e25730c74
Andre Schevz de Werk <andrewerk1804@gmail.com>
2022-06-03 16:27:48 -0300

    Connection with DB restored

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,6 @@
 const errorHandler = (err, _req, res, _next) => {
   const error = JSON.parse(err.message);
+  console.log('Error message:', error.message);
   res.status(error.status || 500).json({ message: error.message });
 };
 
